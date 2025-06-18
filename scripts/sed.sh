0b58f3b8afb6767e5f40d0129df8b2fc0b1d7cf8
Rahul Chandra <rahul@projecteclipse.org>
2025-06-18 11:50:01 -0400

    Remove cvzpf references

--- scripts/sed.sh
+++ scripts/sed.sh
@@ -10,7 +10,7 @@ sed -i 's#./dir.sh##g' $1
 sed -i 's#sudo\ porg\ -lp\ $PACKAGE\ "##g' $1
 sed -i 's#install"#install#g' $1
 sed -i 's#ninja\ install#ninja\ install\ \&\&\ DESTDIR=/pkgs/$PACKAGE\ ninja\ install#g' $1
-sed -i 's/cvpf/cvzpf/g' $1
+sed -i 's/cvpf/cvapf/g' $1
 sed -i 's#/finished/gnome42#/finished#g' $1
 sed -i 's#/gnome#/pkgs#g' $1
 cat $1 | grep gnome
